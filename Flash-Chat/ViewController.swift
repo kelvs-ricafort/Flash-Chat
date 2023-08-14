5f97041e92a3416ec182d9d39605619df9af31a2
Kelvin Ricafort <kelvs.r@outlook.com>
2023-08-14 22:24:09 +0800

    Initial Commit

---
+++ Flash-Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash-Chat
+//
+//  Created by Kelvin Ricafort on 8/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
